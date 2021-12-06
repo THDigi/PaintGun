5de418f1a714ac8ab303658d1c49438ac2ed8ab5
Digi <hunter_digital_sl@yahoo.com>
2021-12-06 08:30:45 +0200

    Internal: gitignore tweaks and added easy copy to release batch

---
+++ Copy to Release folder.bat
@@ -0,0 +1,8 @@
+@echo off
+set REPLACE_IN_PATH=%APPDATA%\SpaceEngineers\Mods\PaintGun
+
+rmdir "%REPLACE_IN_PATH%" /S /Q
+
+robocopy.exe .\ "%REPLACE_IN_PATH%" *.* /S /xd .git bin obj .vs ignored /xf *.exe *.dll *.lnk *.git* *.bat *.zip *.7z *.blend* *.png *.md *.log *.sln *.csproj *.csproj.user *.ruleset desktop.ini *.fbx *.hkt *.xml *.txt *.sbmi
+
+pause
---
+++ Textures/convert aluminium.bat
@@ -0,0 +1,3 @@
+C:\Programs\Texconv\texconv.exe CustomAluminium_cm.png -y -f BC7_UNORM_SRGB -if TRIANGLE_DITHER_DIFFUSION -sepalpha
+C:\Programs\Texconv\texconv.exe CustomAluminium_ng.png -y -f BC7_UNORM -if TRIANGLE_DITHER_DIFFUSION -sepalpha
+ren *.DDS *.dds
