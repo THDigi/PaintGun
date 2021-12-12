d9882c02fa3b582be6634e18e2d9baae44a38f3a
Digi <hunter_digital_sl@yahoo.com>
2021-12-12 07:52:50 +0200

    Tweaked sound volume of HUD sounds to match game's HUD sounds.

--- Data/Scripts/PaintGun/Features/HUDSounds.cs
+++ Data/Scripts/PaintGun/Features/HUDSounds.cs
@@ -1,18 +1,22 @@
-﻿using Sandbox.Game.Entities;
+﻿using System;
+using Sandbox.Game;
+using Sandbox.Game.Entities;
 using Sandbox.ModAPI;
 
 namespace Digi.PaintGun.Features
 {
     public class HUDSounds : ModComponent
     {
+        const float VolumeMultiplier = 0.3f; // HACK: earlobe'd it match the game's HUD sounds
+
         MyEntity3DSoundEmitter SoundEmitter;
         int SoundTimeout = 0;
 
-        readonly MySoundPair SOUND_HUD_UNABLE = new MySoundPair("HudUnable");
-        readonly MySoundPair SOUND_HUD_CLICK = new MySoundPair("HudClick");
-        readonly MySoundPair SOUND_HUD_MOUSE_CLICK = new MySoundPair("HudMouseClick");
-        readonly MySoundPair SOUND_HUD_COLOR = new MySoundPair("HudColorBlock");
-        readonly MySoundPair SOUND_HUD_ITEM = new MySoundPair("HudItem");
+        readonly MySoundPair SoundUnable = new MySoundPair("HudUnable");
+        readonly MySoundPair SoundClick = new MySoundPair("HudClick");
+        readonly MySoundPair SoundMouseClick = new MySoundPair("HudMouseClick");
+        readonly MySoundPair SoundColor = new MySoundPair("HudColorBlock");
+        readonly MySoundPair SoundItem = new MySoundPair("HudItem");
 
         public HUDSounds(PaintGunMod main) : base(main)
         {
@@ -31,31 +35,31 @@ namespace Digi.PaintGun.Features
         public void PlayUnable(bool ignoreSetting = false)
         {
             if(ignoreSetting || Main.Settings.extraSounds)
-                PlayHudSound(SOUND_HUD_UNABLE, 0.5f, timeout: 60);
+                PlayHudSound(SoundUnable, 0.5f, timeout: 60);
         }
 
         public void PlayClick(bool ignoreSetting = false)
         {
             if(ignoreSetting || Main.Settings.extraSounds)
-                PlayHudSound(SOUND_HUD_CLICK, 0.25f);
+                PlayHudSound(SoundClick, 0.25f);
         }
 
         public void PlayMouseClick(bool ignoreSetting = false)
         {
             if(ignoreSetting || Main.Settings.extraSounds)
-                PlayHudSound(SOUND_HUD_MOUSE_CLICK, 0.25f);
+                PlayHudSound(SoundMouseClick, 0.25f);
         }
 
         public void PlayColor(bool ignoreSetting = false)
         {
             if(ignoreSetting || Main.Settings.extraSounds)
-                PlayHudSound(SOUND_HUD_COLOR, 0.8f);
+                PlayHudSound(SoundColor, 0.5f);
         }
 
         public void PlayItem(bool ignoreSetting = false)
         {
             if(ignoreSetting || Main.Settings.extraSounds)
-                PlayHudSound(SOUND_HUD_ITEM, 0.6f);
+                PlayHudSound(SoundItem, 0.5f);
         }
 
         void PlayHudSound(MySoundPair soundPair, float volume, int timeout = 0)
@@ -72,7 +76,7 @@ namespace Digi.PaintGun.Features
             {
                 SoundEmitter = new MyEntity3DSoundEmitter(null);
 
-                // remove all effects and conditions from this emitter
+                // remove all effects and conditions from this emitter; must not clear the dictionary itself!
                 SoundEmitter.EmitterMethods[(int)MyEntity3DSoundEmitter.MethodsEnum.CanHear].ClearImmediate();
                 SoundEmitter.EmitterMethods[(int)MyEntity3DSoundEmitter.MethodsEnum.ShouldPlay2D].ClearImmediate();
                 SoundEmitter.EmitterMethods[(int)MyEntity3DSoundEmitter.MethodsEnum.CueType].ClearImmediate();
@@ -80,7 +84,7 @@ namespace Digi.PaintGun.Features
             }
 
             SoundEmitter.SetPosition(MyAPIGateway.Session.Camera.WorldMatrix.Translation);
-            SoundEmitter.CustomVolume = volume;
+            SoundEmitter.CustomVolume = volume * VolumeMultiplier;
             SoundEmitter.PlaySound(soundPair, stopPrevious: false, alwaysHearOnRealistic: true, force2D: true);
         }
     }
